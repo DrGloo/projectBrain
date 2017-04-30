0107fa72d1a337cd6eb0c78cb3b6fa3ed4a07329
DrGloo <raishanbernard@gmail.com>
2017-04-30 06:14:44 +0000

    v0.01

---
+++ script.js
@@ -0,0 +1,14 @@
+var myName = window.prompt("Whats your name?")
+$("#navtext1").html("Welcome, " + myName);
+$("#question").html("Welcome back " + myName);
+$("#question").addClass("animated fadeInRightBig");
+$("#question").css("opacity", "0.5")
+
+
+
+
+
+
+
+
+console.log("Script is running")
